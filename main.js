60786ada79c3ac901ee69b0dea5a2f7fde06622a
Wassim DHIF <wassimdhif@gmail.com>
2018-04-05 12:18:35 +0200

    Base code added
    From Screeps starter
    - Main
    - Builder role
    - Harvester role
    - Upgrader role
    
    Signed-off-by: Wassim DHIF <wassimdhif@gmail.com>

---
+++ main.js
@@ -0,0 +1,34 @@
+const roleHarvester = require('role.harvester');
+const roleUpgrader = require('role.upgrader');
+const roleBuilder = require('role.builder');
+
+module.exports.loop = () => {
+
+    let tower = Game.getObjectById('16f1b00c6fdf2d29dc7250d5');
+    for (let towerName in Game.towers) {
+        let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for (let name in Game.creeps) {
+        let creep = Game.creeps[name];
+        if (creep.memory.role === 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role === 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role === 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+};
---
+++ role.harvester.js
@@ -0,0 +1,37 @@
+let roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: (creep) => {
+
+        if (creep.carry.energy < creep.carryCapacity) {
+            let sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {
+                    visualizePathStyle: {
+                        stroke: '#ffaa00'
+                    }
+                });
+            }
+        }
+        else {
+            let targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType === STRUCTURE_EXTENSION ||
+                        structure.structureType === STRUCTURE_SPAWN ||
+                        structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {
+                        visualizePathStyle: {
+                            stroke: '#ffffff'
+                        }
+                    });
+                }
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester;
