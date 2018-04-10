52752932d016478e6eb8afac9fe81b15ab4722cc
Wassim DHIF <wassimdhif@gmail.com>
2018-04-10 14:57:45 +0200

    Add population constraints for all roles
    Add spawning check before spawning
    Add energy check before spawning

--- population.js
+++ population.js
@@ -1,16 +1,32 @@
+const POPULATION = {
+    'harvester': 10,
+    'builder': 3,
+    'upgrader': 2
+};
+
+const ROLES = {
+    'harvester': [WORK, CARRY, MOVE, MOVE],
+    'builder': [WORK, CARRY, MOVE, MOVE], // TODO Add correct body
+    'upgrader': [WORK, CARRY, MOVE, MOVE], // TODO Add correct body
+};
+
 let population = {
 
     run: () => {
-        const HARVESTER_POPULATION = 10;
-        let numberOfHarvester = _.sum(Game.creeps, (creep) => creep.memory.role === 'harvester')
+        for (let key in POPULATION) {
+            let currentPopulation = _.sum(Game.creeps, (creep) => creep.memory.role === key);
+            if (currentPopulation < POPULATION[key]) {
+                let creepName = Math.random().toString();
 
-        if (numberOfHarvester < HARVESTER_POPULATION) {
-            let creepName = Math.random().toString();
+                let spawnHasEnoughEnergy = Game.spawns['Spawn1'].spawnCreep(ROLES[key], creepName, {dryRun: true});
 
-            Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, MOVE], creepName, {
-                memory: {role: 'harvester'}
-            });
-            console.log('Spawned a new Harvester: ' + creepName)
+                if (!Game.spawns['Spawn1'].spawning && spawnHasEnoughEnergy !== -6) {
+                    Game.spawns['Spawn1'].spawnCreep(ROLES[key], creepName, {
+                        memory: {role: key}
+                    });
+                    console.log(`Spawned a new ${key}: ${creepName}`);
+                }
+            }
         }
     }
 };
